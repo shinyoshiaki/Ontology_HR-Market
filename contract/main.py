4c7b713fdc8133d006c5a5557c1f08004fe3d08b
iida-hayato <hayato.iida.0213@gmail.com>
2018-12-09 12:47:22 +0900

    merge token code

--- contract/main.py
+++ contract/main.py
@@ -3,10 +3,24 @@ from boa.interop.System.Runtime import Notify, Serialize, Deserialize, GetTime, 
 from boa.builtins import concat
 from boa.interop.Ontology.Native import Invoke
 
-MESIToken = RegisterAppCall('fa80553aec57a57d406cde1549b47f0bad96f7c4', 'operation', 'args')
+MESIToken = RegisterAppCall('d92a87ee43c7dd227b327c06d0f1ba1555d1a4d3', 'operation', 'args')
 
 ctx = GetContext()
-contractAddress = bytearray(b'\xc4\xf7\x96\xad\x0b\x7f\xb4\x49\x15\xde\x6c\x40\x7d\xa5\x57\xec\x3a\x55\x80\xfa')
+contractAddress = bytearray(b'\xd3\xa4\xd1\x55\x15\xba\xf1\xd0\x06\x7c\x32\x7b\x22\xdd\xc7\x43\xee\x87\x2a\xd9')
+
+
+NAME = 'MESItoken'
+SYMBOL = 'MST'
+DECIMAL = 8
+FACTOR = 100000000
+OWNER = ToScriptHash("Ad7PyMYUYx5GdpvC9Sw3tK674hHrTRPT5C")
+TOTAL_AMOUNT = 1000000000
+
+TRANSFER_PREFIX = bytearray(b'\x01')
+APPROVE_PREFIX = bytearray(b'\x02 ')
+
+SUPPLY_KEY = 'totoalSupply'
+
 
 def Main(operation, args):
 
@@ -39,6 +53,54 @@ def Main(operation, args):
     
     if operation == 'transfer':
         return transfer(args[0],args[1],args[2])
+
+
+    if operation == 'name':
+        return Name()
+    if operation == 'totalSupply':
+        return TotalSupply()
+    if operation == 'init':
+        return init()
+    if operation == 'symbol':
+        return Symbol()
+    if operation == 'transfer':
+        if len(args) != 3:
+            return False
+        else:
+            from_acct = args[0]
+            to_acct = args[1]
+            amount = args[2]
+            return transfer(from_acct, to_acct, amount)
+    if operation == 'transferMulti':
+        return TransferMulti(args)
+    if operation == 'approve':
+        if len(args) != 3:
+            return False
+        owner = args[0]
+        spender = args[1]
+        amount = args[2]
+        return Approve(owner, spender, amount)
+    if operation == 'transferFrom':
+        if len(args) != 4:
+            return False
+        spender = args[0]
+        from_acct = args[1]
+        to_acct = args[2]
+        amount = args[3]
+        return TransferFrom(spender, from_acct, to_acct, amount)
+    if operation == 'balanceOf':
+        if len(args) != 1:
+            return False
+        acct = args[0]
+        return balanceOf(acct)
+    if operation == 'decimal':
+        return Decimal()
+    if operation == 'allowance':
+        if len(args) != 2:
+            return False
+        owner = args[0]
+        spender = args[1]
+        return Allowance(owner, spender)
 
     return False
 
@@ -214,18 +276,6 @@ def getHighestBid(personAddr):
     return Deserialize(Get(ctx, concat('highest_bid_',personAddr)))
     
 
-def transfer(fromacct, toacct, amount):
-
-    res = MESIToken( 'transfer', [fromacct, toacct, amount])
-    Notify(res)
-
-    if res and res == b'\x01':
-        Notify('transfer succeed')
-        return True
-    else:
-        Notify('transfer failed')
-
-        return False
 
 
 def makeState(fromacct, toacct, amount):
@@ -248,3 +298,123 @@ def makeValue(values, divider):
         val = concat(val, values[i])
         
     return val
+    
+    
+
+def Symbol():
+    return SYMBOL
+
+
+def Name():
+    return NAME
+
+
+def Decimal():
+    return DECIMAL
+
+
+def TotalSupply():
+    return TOTAL_AMOUNT * FACTOR
+
+
+def init():
+    if Get(ctx, SUPPLY_KEY):
+        Notify('Already initialized!')
+        return False
+    else:
+        total = TOTAL_AMOUNT * FACTOR
+        Put(ctx, SUPPLY_KEY, total)
+        Put(ctx, concat(TRANSFER_PREFIX, OWNER), total)
+        Notify(['transfer', '', OWNER, total])
+        return True
+
+
+def transfer(from_acct, to_acct, amount):
+    if from_acct == to_acct:
+        return True
+    if amount == 0:
+        return True
+    if amount < 0:
+        return False
+#    if CheckWitness(from_acct) == False:
+#        return False
+    fromKey = concat(TRANSFER_PREFIX, from_acct)
+    fromBalance = Get(ctx, fromKey)
+    if fromBalance < amount:
+        return False
+    if fromBalance == amount:
+        Delete(ctx, fromKey)
+    else:
+        Put(ctx, fromKey, fromBalance - amount)
+
+    tokey = concat(TRANSFER_PREFIX, to_acct)
+    toBalance = Get(ctx, tokey)
+
+    Put(ctx, tokey, toBalance + amount)
+    Notify(['transfer', from_acct, to_acct, amount])
+    return True
+
+
+def TransferMulti(args):
+    for p in (args):
+        if len(p) != 3:
+            return False
+        if transfer(p[0], p[1], p[2]) == False:
+            # return False #  wrong since the previous transaction will be successful
+            raise Exception("TransferMulti failed.")
+    return True
+
+
+def Approve(owner, spender, amount):
+    if amount < 0:
+        return False
+    if CheckWitness(owner) == False:
+        return False
+    if len(spender) != 20:
+        return False
+    key = concat(concat(APPROVE_PREFIX, owner), spender)
+    Put(ctx, key, amount)
+    Notify(['approve', owner, spender, amount])
+    return True
+
+
+def TransferFrom(sender, from_acct, to_acct, amount):
+    if amount < 0:
+        return False
+    if CheckWitness(sender) == False:
+        return False
+    if len(to_acct) != 20:
+        return False
+    appoveKey = concat(concat(APPROVE_PREFIX, from_acct), sender)
+    approvedAmount = Get(ctx, appoveKey)
+    if approvedAmount < amount:
+        return False
+    if approvedAmount == amount:
+        Delete(ctx, appoveKey)
+    else:
+        Put(ctx, appoveKey, approvedAmount - amount)
+
+    fromKey = concat(TRANSFER_PREFIX, from_acct)
+    fromBalance = Get(ctx, fromKey)
+    if fromBalance < amount:
+        return False
+    if fromBalance == amount:
+        Delete(ctx, fromKey)
+    else:
+        Put(ctx, fromKey, fromBalance - amount)
+
+    tokey = concat(TRANSFER_PREFIX, to_acct)
+    toBalance = Get(ctx, tokey)
+
+    Put(ctx, tokey, toBalance + amount)
+    Notify(['transfer', from_acct, to_acct, amount])
+    return True
+
+
+def balanceOf(account):
+    return Get(ctx, concat(TRANSFER_PREFIX, account))
+
+
+def Allowance(owner, spender):
+    allowanceKey = concat(concat(APPROVE_PREFIX, owner), spender)
+    return Get(ctx, allowanceKey)
